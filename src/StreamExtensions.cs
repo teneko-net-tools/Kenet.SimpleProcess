a8b94c59b5b932751f1267cfd701e0d99053ba3a
teneko <giteroneko@gmail.com>
2022-11-16 07:42:28 +0000

    Added GitHub workflow and added support for net5.0

--- src/StreamExtensions.cs
+++ src/StreamExtensions.cs
@@ -2,7 +2,7 @@
 
 internal static class StreamExtensions
 {
-#if !NET6_0_OR_GREATER
+#if !NET5_0_OR_GREATER
     /// <inheritdoc cref="CommunityToolkit.HighPerformance.StreamExtensions.ReadAsync(Stream, Memory{byte}, CancellationToken)"/>
     public static ValueTask<int> ReadAsync(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default) =>
         CommunityToolkit.HighPerformance.StreamExtensions.ReadAsync(stream, buffer, cancellationToken);
