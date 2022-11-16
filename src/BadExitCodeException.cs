917fdb50de7aef1c9f9064f4f43ba101841cf676
teneko <giteroneko@gmail.com>
2022-11-16 07:32:23 +0000

    Intitial commit

---
+++ src/BadExitCodeException.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+
+namespace Kenet.SimpleProcess
+{
+    /// <summary>
+    /// An exception that indicates a bad exit code.
+    /// </summary>
+    public class BadExitCodeException : Exception
+    {
+        /// <summary>
+        /// The bad exit code.
+        /// </summary>
+        public int? ExitCode { get; init; }
+
+        /// <inheritdoc/>
+        public override string Message => (base.Message?.Length == 0 ? "The process exited with bad exit code" : base.Message)
+           + (ExitCode != null ? $"{Environment.NewLine}Exit Code = {ExitCode}" : string.Empty);
+
+        /// <inheritdoc/>
+        public BadExitCodeException()
+        {
+        }
+
+        /// <inheritdoc/>
+        public BadExitCodeException(string? message) : base(message)
+        {
+        }
+
+        /// <inheritdoc/>
+        public BadExitCodeException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected BadExitCodeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/IsExternalInit.cs
@@ -0,0 +1,12 @@
+﻿# if !NET6_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage, DebuggerNonUserCode]
+    internal static class IsExternalInit
+    {
+    }
+}
+#endif
