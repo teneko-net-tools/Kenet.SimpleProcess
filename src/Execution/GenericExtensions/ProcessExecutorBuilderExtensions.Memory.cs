9732f17b6ac7d275b9537ae94b6c8af85b7bef9d
teneko <giteroneko@gmail.com>
2022-11-19 01:31:16 +0000

    Added support for possible void returning methods

--- src/Execution/GenericExtensions/ProcessExecutorBuilderExtensions.Memory.cs
+++ src/Execution/GenericExtensions/ProcessExecutorBuilderExtensions.Memory.cs
@@ -4,7 +4,7 @@ public static partial class ProcessExecutorBuilderExtensions
 {
     public static T WriteTo<T>(
         this T builder,
-        Func<T, Func<WriteHandler, object>> readFrom,
+        Func<T, Action<WriteHandler>> readFrom,
         Memory<byte> memory,
         int startIndex)
         where T : IProcessExecutorBuilder
@@ -22,7 +22,30 @@ public static partial class ProcessExecutorBuilderExtensions
     public static T WriteTo<T>(
         this T builder,
         Func<T, Func<WriteHandler, object>> readFrom,
+        Memory<byte> memory,
+        int startIndex)
+        where T : IProcessExecutorBuilder
+    {
+        var writerHandler = readFrom(builder);
+        void WriteHandler(WriteHandler bytes) => _ = writerHandler(bytes);
+        return WriteTo(builder, _ => WriteHandler, memory, startIndex);
+    }
+
+    public static T WriteTo<T>(
+        this T builder,
+        Func<T, Action<WriteHandler>> readFrom,
         Memory<byte> memory)
         where T : IProcessExecutorBuilder =>
         builder.WriteTo(readFrom, memory, startIndex: 0);
+
+    public static T WriteTo<T>(
+        this T builder,
+        Func<T, Func<WriteHandler, object>> readFrom,
+        Memory<byte> memory)
+        where T : IProcessExecutorBuilder
+    {
+        var writerHandler = readFrom(builder);
+        void WriteHandler(WriteHandler bytes) => _ = writerHandler(bytes);
+        return WriteTo(builder, _ => WriteHandler, memory);
+    }
 }
