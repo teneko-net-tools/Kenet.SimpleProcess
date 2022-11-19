29b0cff77f6781db38a3845d6ccef11d19659c23
teneko <giteroneko@gmail.com>
2022-11-19 01:31:16 +0000

    Added support for possible void returning methods

--- src/Execution/GenericExtensions/ProcessExecutorBuilderExtensions.IBufferWriter.cs
+++ src/Execution/GenericExtensions/ProcessExecutorBuilderExtensions.IBufferWriter.cs
@@ -7,11 +7,42 @@ public static partial class ProcessExecutorBuilderExtensions
 {
     public static T WriteTo<T>(
         this T builder,
+        Func<T, Action<WriteHandler>> readFrom,
+        IBufferWriter<byte> bufferWriter)
+        where T : IProcessExecutorBuilder
+    {
+        readFrom(builder)(bufferWriter.Write);
+        return builder;
+    }
+
+    public static T WriteTo<T>(
+        this T builder,
         Func<T, Func<WriteHandler, object>> readFrom,
         IBufferWriter<byte> bufferWriter)
         where T : IProcessExecutorBuilder
     {
-        _ = readFrom(builder)(bufferWriter.Write);
+        var writerHandler = readFrom(builder);
+        void WriteHandler(WriteHandler bytes) => _ = writerHandler(bytes);
+        return WriteTo(builder, _ => WriteHandler, bufferWriter);
+    }
+
+    /// <summary>
+    /// Writes to <param name="memoryOwner" />. The memory MUST be released on your own!
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="readFrom"></param>
+    /// <param name="memoryOwner"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T WriteTo<T>(
+        this T builder,
+        Func<T, Action<WriteHandler>> readFrom,
+        out IMemoryOwner<byte> memoryOwner)
+        where T : IProcessExecutorBuilder
+    {
+        var buffer = new ArrayPoolBufferWriter<byte>();
+        memoryOwner = buffer;
+        readFrom(builder)(buffer.Write);
         return builder;
     }
 
@@ -29,9 +60,8 @@ public static partial class ProcessExecutorBuilderExtensions
         out IMemoryOwner<byte> memoryOwner)
         where T : IProcessExecutorBuilder
     {
-        var buffer = new ArrayPoolBufferWriter<byte>();
-        memoryOwner = buffer;
-        _ = readFrom(builder)(buffer.Write);
-        return builder;
+        var writerHandler = readFrom(builder);
+        void WriteHandler(WriteHandler bytes) => _ = writerHandler(bytes);
+        return WriteTo(builder, _ => WriteHandler, out memoryOwner);
     }
 }
